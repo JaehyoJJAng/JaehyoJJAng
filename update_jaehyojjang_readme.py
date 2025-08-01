aacf926b70cc795766e7e914804ac3d1cf6b598c
JaehyoJJAng <yshrim12@naver.com>
2025-08-01 07:30:28 +0000

    link encoding error fixed

--- update_jaehyojjang_readme.py
+++ update_jaehyojjang_readme.py
@@ -17,14 +17,16 @@ CUSTOM_MARKDOWN: str = """
 ## ✒️ Recent Blog Posts
 """
 
-for idx, feed in enumerate(RSS_FEED['entries']):
+for idx, feed in enumerate(RSS_FEED["entries"]):
     if idx > MAX_POST:
         break
-    feed_date = datetime.fromtimestamp(time.mktime(feed['published_parsed'])) + timedelta(hours=9)
-    CUSTOM_MARKDOWN+= f"[{feed_date.strftime('%Y.%m.%d')} - {feed['title']}]({feed['link']}) <br/>\n"
+    feed_date = datetime.fromtimestamp(
+        time.mktime(feed["published_parsed"])
+    ) + timedelta(hours=9)
+    CUSTOM_MARKDOWN += f"[{feed_date.strftime('%Y.%m.%d')} - {feed['title']}]({feed['link'].replace(' ', '%20')}) <br/>\n"
 
-NOW_DATE = ( datetime.now() + timedelta(hours=9) ).strftime('%Y/%m/%d_%H:%M')
-CUSTOM_MARKDOWN+=f'\n\n<img src=\"https://img.shields.io/badge/최근%20배포일-{NOW_DATE}-%23121212?style=flat\">'
+NOW_DATE = (datetime.now() + timedelta(hours=9)).strftime("%Y/%m/%d_%H:%M")
+CUSTOM_MARKDOWN += f'\n\n<img src="https://img.shields.io/badge/최근%20배포일-{NOW_DATE}-%23121212?style=flat">'
 
-with open('README.md', mode='w', encoding='utf-8') as f: 
+with open("README.md", mode="w", encoding="utf-8") as f:
     f.write(CUSTOM_MARKDOWN)
